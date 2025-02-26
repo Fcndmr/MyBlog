a32b80ce5efb5450cfa1ddd574379bb279ba8775
Efe Can Demir <efecandemir7887@gmail.com>
2025-02-26 17:28:41 +0300

    A few changes in color component added

--- frontend/src/components/color/Color.jsx
+++ frontend/src/components/color/Color.jsx
@@ -5,15 +5,16 @@ function Color() {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
+  const [alpha, setAlpha] = useState(1);
 
   return (
     <div className="cont">
       <div className="container">
         <div
           className="box"
-          style={{ background: `rgb(${red},${green},${blue})` }}
+          style={{ background: `rgba(${red},${green},${blue},${alpha})` }}
         >
-          {`${red}, ${green}, ${blue}`}
+          {`${red}, ${green}, ${blue}, ${alpha}`}
         </div>
         <div className="controls">
           <div className="control-group">
@@ -23,7 +24,7 @@ function Color() {
               min="0"
               max="255"
               value={red}
-              onChange={(e) => setRed(e.target.value)}
+              onChange={(e) => setRed(Number(e.target.value))}
             />
           </div>
           <div className="control-group">
@@ -33,7 +34,7 @@ function Color() {
               min="0"
               max="255"
               value={green}
-              onChange={(e) => setGreen(e.target.value)}
+              onChange={(e) => setGreen(Number(e.target.value))}
             />
           </div>
           <div className="control-group">
@@ -43,7 +44,18 @@ function Color() {
               min="0"
               max="255"
               value={blue}
-              onChange={(e) => setBlue(e.target.value)}
+              onChange={(e) => setBlue(Number(e.target.value))}
+            />
+          </div>
+          <div className="control-group">
+            <label>Alpha</label>
+            <input
+              type="range"
+              min="0"
+              max="1"
+              step="0.01"
+              value={alpha}
+              onChange={(e) => setAlpha(parseFloat(e.target.value))}
             />
           </div>
         </div>
